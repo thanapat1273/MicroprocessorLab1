3ddd23fbcee862c55311eb7abde9e41c7fc859f8
thanapat1273 <60319434+thanapat1273@users.noreply.github.com>
2020-02-02 20:31:30 +0700

    Add files via upload

---
+++ MicroLab2-2.ino
@@ -0,0 +1,53 @@
+int clk_pin = 13;
+int latch_pin = 10;
+int data_pin = 11;
+bool toggle;
+int count = 0;
+int potNum = 0;
+int LB;
+int RB;
+
+void setLED(uint16_t state)
+{
+  digitalWrite(latch_pin,LOW);
+  for(int i=0; i<16; i++)
+  {
+    digitalWrite(data_pin, (state>>(15-i)) & 0x0001);
+    digitalWrite(clk_pin,HIGH);
+    digitalWrite(clk_pin,LOW);
+  }
+  digitalWrite(latch_pin,HIGH);
+}
+
+void setup() {
+  pinMode(clk_pin,OUTPUT); 
+  pinMode(latch_pin,OUTPUT); 
+  pinMode(data_pin,OUTPUT);
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() { 
+  LB = analogRead(A0);
+  RB = analogRead(A1);
+  count=0;
+  if(LB < 100 && RB > 100)
+  {
+    for(int i=0; i<16; i++)
+    {
+      setLED(0b0000000000000001<<count);
+      count++;
+      delay(200); 
+    }
+  }
+  else if(LB > 100 && RB < 100)
+  {
+    for(int i=0; i<16; i++)
+    {
+      setLED(0b1000000000000000>>count);
+      count++;
+      delay(200);
+    }
+  }
+}
